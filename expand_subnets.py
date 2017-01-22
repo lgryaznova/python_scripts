925d914c6ffc67ae95aeff9444bedf4d881ebd05
zimash <zimash@gmail.com>
2017-01-22 17:03:32 +0300

    something happened

--- expand_subnets.py
+++ expand_subnets.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 This module takes a list of IPv4 subnets from a file specified by the user (with
 each subnet on a new line), gets a list of usable host addresses of each subnet
@@ -15,9 +17,12 @@ located in this folder, too.
 """
 import ipaddress
 import os
+import sys
+
 
 # user interface
-print('\n\
+def usage():
+    print('\n\
 *****************************************************************\n\
 This module takes a list of IPv4 subnets from a file\n\
 specified by the user (with each subnet on a new line),\n\
@@ -31,57 +36,11 @@ If the input file contains info other than IPv4 addresses,\n\
 only IPv4 addresses will be processed and other lines will\n\
 be omitted with the error message displayed as well.\n\n\
 The script must be in the same folder as the input file,\n\
-the output file will be located in it, too.\n\
+the output file will be located in it, too.\n\n\
+Usage: ./expand_subnet.py <file_with_subnets>\n\
+file_with_subnets - name of the file with a list of IPv4 subnets\n\
 *****************************************************************\n')
 
-# ask for and validate user input (input file)
-f_input = input('Enter the name of the file with a list of IPv4 subnets: ')
-while not os.path.exists(f_input):
-    f_input = input('File not found. Enter the correct filename \
-or press Ctrl-C to exit: ')
-
-# ask for and validate user input (output file)    
-f_output = input('Enter the name for the output file: ')
-choice = ''
-while choice != 'y':
-    if os.path.exists(f_output):
-        choice = input('File already exists. Do you want to overwrite it? y/n ')
-        if choice == 'n':
-            f_output = input('Enter the name for the output file: ')
-        elif choice != 'y':
-            print('Cannot recognize your answer. To exit script press Ctrl-C.')
-    else:
-        break
-print('\nProcessing...\n')
-
-# read in the list of subnets
-netlist = []
-with open(f_input, 'r') as tempfile:
-    for line in tempfile:
-        netlist.append(line.strip(' \n\t'))
-
-# convert all items in the list into valid subnet addresses, mark errors
-invalid_lines = []
-for i, elem in enumerate(netlist):
-    try:
-        netlist[i] = ipaddress.ip_network(elem, strict=True)
-    except ValueError as ex1:
-        # if it's a host address, get the network address and log warning
-        try:
-            netlist[i] = ipaddress.ip_network(elem, strict=False)
-            print('Warning: {0}. {1} is used instead.'.format(str(ex1), \
-str(netlist[i].network_address)))
-        # if not an IPv4 address, mark as error and log the error
-        except ValueError as ex2:
-            invalid_lines.append(i)
-            # empty strings are omitted without logging
-            if netlist[i] != '':
-                print('Invalid subnet address: {0}.'.format(str(ex2)))
-
-# delete invalid elements of the list of subnets
-if invalid_lines:
-    for i in invalid_lines[::-1]:
-        del netlist[i]
 
 # generator to be used to write directly to the output file to save memory
 def hosts_generator(input_list):
@@ -95,8 +54,59 @@ def hosts_generator(input_list):
         for i in subnet.hosts():
             yield str(i) + '\n'
 
-# write output file
-with open(f_output, 'w') as outfile:
-    outfile.writelines(hosts_generator(netlist))
 
-print('\nDone!\n')
+def body(f_input):
+    f_output = f_input + ".out"
+    choice = ''
+    while choice != 'y':
+        if os.path.exists(f_output):
+            choice = input('File already exists. Do you want to overwrite it? y/n ')
+            if choice == 'n':
+                f_output = input('Enter the name for the output file: ')
+            elif choice != 'y':
+                print('Cannot recognize your answer. To exit script press Ctrl-C.')
+        else:
+            break
+    print('\nProcessing...\n')
+
+    # read in the list of subnets
+    netlist = []
+    with open(f_input, 'r') as tempfile:
+        for line in tempfile:
+            netlist.append(line.strip(' \n\t'))
+
+    # convert all items in the list into valid subnet addresses, mark errors
+    invalid_lines = []
+    for i, elem in enumerate(netlist):
+        try:
+            netlist[i] = ipaddress.ip_network(elem, strict=True)
+        except ValueError as ex1:
+            # if it's a host address, get the network address and log warning
+            try:
+                netlist[i] = ipaddress.ip_network(elem, strict=False)
+                print('Warning: {0}. {1} is used instead.'.format(
+                    str(ex1), str(netlist[i].network_address)))
+            # if not an IPv4 address, mark as error and log the error
+            except ValueError as ex2:
+                invalid_lines.append(i)
+                # empty strings are omitted without logging
+                if netlist[i] != '':
+                    print('Invalid subnet address: {0}.'.format(str(ex2)))
+
+    # delete invalid elements of the list of subnets
+    if invalid_lines:
+        for i in invalid_lines[::-1]:
+            del netlist[i]
+
+    # write output file
+    with open(f_output, 'w') as outfile:
+        outfile.writelines(hosts_generator(netlist))
+
+    print('\nDone!\n')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
+        body(sys.argv[1])
+    else:
+        usage()
